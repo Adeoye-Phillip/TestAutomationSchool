60cb5d58b1453a3e824238a36c77fd6ca25873b8
AdePhillipz <phillip4peace@gmail.com>
2024-02-02 00:53:55 +0100

    new update

---
+++ module 3(JS)/app12.js
@@ -0,0 +1,20 @@
+// Creating Function in JavaScript
+function MyFunction() {     // function declaration
+    console.log("My first function")
+}
+MyFunction();   // function call
+
+
+// Function, Parameter, and Argument
+function greeting(name) {  // name here is the parameter
+    console.log("Good morning, " + name + '!') // This is the function body
+}
+greeting('Adeoye'); // Adeoye here is the argument (Return value)
+
+
+// A Function that add two numbers together
+function addNumber(firstNumber, secondNumber) {
+    const sum = firstNumber + secondNumber;
+    console.log(sum);
+}
+addNumber(30, 40);
---
+++ module 3(JS)/task_9.js
@@ -0,0 +1,11 @@
+// Write JavaScript function, calculateAreaOfRectangle. The function should
+// Take two parameters, length and width. 
+// Multiply the length and width
+// Log the product to the console
+
+
+function calculateAreaOfRectangle(length, width) {
+    const multiply = length * width
+    console.log(multiply);
+}
+calculateAreaOfRectangle(10, 10);
